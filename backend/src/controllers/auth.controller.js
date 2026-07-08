e5a5d94d66436c1e883aee578d63356ef7edb276
B.Pradhan <bpradhan17120@gmail.com>
2026-07-08 11:36:10 +0530

    feat: implement duplicate registration checks and secure login logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -28,7 +28,7 @@ async function registerUser(req, res) {
     const token = jwt.sign({
         id: user._id,
 
-    },"66fd434132c747ee8d4075659e9c2bed0d955b7b")
+    }, process.env.JWT_SECRET)
 
     res.cookie("token", token)
 
@@ -45,7 +45,44 @@ async function registerUser(req, res) {
 
 
 
-async function loginUser(req, res) {}
+async function loginUser(req, res) {
+
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ 
+        email 
+    })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message:"Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+    
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
+
+
+}
 
 module.exports = {
     registerUser,
